fe4cc752a287260d050ea725d9ada64afc9841c6
apvalkov <a.valkov@inxy.com>
2024-05-23 10:46:16 +0300

    fix composer.lock, fix test

--- tests/SignatureSDKTest.php
+++ tests/SignatureSDKTest.php
@@ -15,11 +15,12 @@ class SignatureSDKTest extends TestCase
      */
     public function testSuccessSign(): void
     {
-        $signatureSDK = new SignatureSDK(PrivateKeyFixture::string());
-        $message      = 'Hello test';
-        $result       = $signatureSDK->signMessage($message);
-        $publicKey    = openssl_pkey_get_public(PublicKeyFixture::string());
-        $verifyResult = openssl_verify(strtolower($message) . '_' . $result->time, base64_decode($result->signature), $publicKey, OPENSSL_ALGO_SHA256);
+        $signatureSDK    = new SignatureSDK(PrivateKeyFixture::string());
+        $message         = 'Hello test';
+        $result          = $signatureSDK->signMessage($message);
+        $publicKeyString = PublicKeyFixture::string();
+        $publicKey       = openssl_pkey_get_public($publicKeyString);
+        $verifyResult    = openssl_verify(strtolower($message) . '_' . $result->time, base64_decode($result->signature), $publicKey, OPENSSL_ALGO_SHA256);
 
         $this->assertEquals(1, $verifyResult);
     }
